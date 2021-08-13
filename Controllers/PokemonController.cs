629d2b541645e63c5b60fceb599cdcd5350e8e7b
asinclair19 <46140300+asinclair19@users.noreply.github.com>
2021-08-12 23:44:51 -0600

    first commit

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Dapper;
+
+namespace api_rest_pokemon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        //connection string
+        private string _connection = @"Server=bicadwf2tm0arkjefpgd-mysql.services.clever-cloud.com;Database=bicadwf2tm0arkjefpgd; Uid=u65f1nd5r5jgkomb; Pwd=jWXkdGeD8zkFP1D3CbWq";
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<Models.Pokemon> lst = null;
+            using (var database = new MySqlConnection(_connection))
+            {
+                var sql_query = "SELECT * FROM pokemon;";
+
+                lst = database.Query<Models.Pokemon>(sql_query);
+            }
+
+            return Ok(lst);
+        }
+
+        [HttpPost]
+        public IActionResult Insert(Models.Pokemon model)
+        {
+            int result = 0;
+
+            using (var database = new MySqlConnection(_connection))
+            {
+                var sql_query = "INSERT INTO pokemon(id,name,base_experience,height,img,abilities,moves,stats)" +
+                    " VALUES(@id,@name,@base_experience,@height,@img,@abilities,@moves,@stats)";
+                result = database.Execute(sql_query, model);
+            }
+            return Ok(result);
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            int result = 0;
+
+            using (var database = new MySqlConnection(_connection))
+            {
+                var sql_query = $"DELETE FROM pokemon WHERE id={id}";
+                result = database.Execute(sql_query);
+            }
+            return Ok(result);
+
+        }
+    }
+}
---
+++ Models/Pokemon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_rest_pokemon.Models
+{
+    public class Pokemon
+    {
+        public int Idsaved { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Base_experience { get; set; }
+        public int Height { get; set; }
+        public string Img { get; set; }
+        public string Abilities { get; set; }
+        public string Moves { get; set; }
+        public string Stats { get; set; }
+    }
+}
